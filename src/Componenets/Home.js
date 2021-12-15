ed55fc27e3d1028e82496de300021d5d8e6c4a75
Aaron <aaron.litton18@gmail.com>
2021-12-15 18:29:43 -0500

    Fixed Typo

--- src/Componenets/Home.js
+++ src/Componenets/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 
 
 
-export default function Home() {
-    return <h1>Welcome user</h1>
+export default function Home(props) {
+    return (
+        !props ? <Navigate to="/login" /> :
+        <h1>Welcome, {props}!</h1>
+    )
 }
