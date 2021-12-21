e61146f5b83a82dfdb4e40beda72a0e37d8d2d83
Aaron <aaron.litton18@gmail.com>
2021-12-20 19:03:55 -0500

    Products can be searched and displayed

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 import Home from './Components/Home'
 import Login from './Components/PreLogin/Login';
 import Register from './Components/PreLogin/Register';
-import Products from './Components/ProductRenderer/Products';
-import Locations from './Components/Location/Locations';
+import Products from './ProductRenderer/Products';
+import Locations from './Components/Locations';
 import GroceryLists from './Components/GroceryLists';
 import useToken from './Models/Token.js';
 import LocationsRenderer from './Components/Location/LocationsRenderer';
